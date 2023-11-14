a994b38a310542567edc8016271eacb3155ed15d
cowboysj <su10jin11@khu.ac.kr>
2023-11-15 01:01:05 +0900

    ad button 수정

--- src/Components/AdPage.jsx
+++ src/Components/AdPage.jsx
@@ -1,5 +1,18 @@
 import React, { Component } from "react";
 import Ad from "./Ad";
+import styled from "styled-components";
+
+const Button = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1.5px solid black;
+  border-radius: 15px;
+  width: 100px;
+  height: 30px;
+  margin: 5px;
+  font-weight: 600;
+`;
 
 class AdPage extends Component {
   constructor(props) {
@@ -19,9 +32,9 @@ class AdPage extends Component {
     return (
       <div>
         <Ad showAd={this.state.showAd} />
-        <button onClick={this.handleToggleClick}>
+        <Button onClick={this.handleToggleClick}>
           {this.state.showAd ? "광고 숨기기" : "광고 보기"}
-        </button>
+        </Button>
       </div>
     );
   }
